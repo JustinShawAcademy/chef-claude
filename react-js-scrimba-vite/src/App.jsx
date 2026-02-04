e803e04796675142821e379e61f7bdc8b10e1cff
Justin Shaw <justinshawcareer@gmail.com>
2026-02-04 14:45:09 -0500

    init: Chef Claude project

--- react-js-scrimba-vite/src/App.jsx
+++ react-js-scrimba-vite/src/App.jsx
@@ -1,27 +1,7 @@
-import Header from "./components/Header/Header.jsx"
-import Entry from "./components/Entry/Entry.jsx"
-import data from "./data/data.js"
+import Header from './components/Header/Header.jsx'
 
 export default function App() {
-
-    const entryElemnts = data.map((entry) => {
-        return <Entry 
-                    img = {{
-                        src: entry.img.src,
-                        alt: entry.img.alt
-                    }}
-                    title = {entry.title}
-                    country = {entry.country}
-                    googleMapsLink = {entry.googleMapsLink}
-                    dates = {entry.dates}
-                    text = {entry.text}
-                />
-    }) 
-
     return (
-        <>
-            <Header />
-            {entryElemnts}
-        </>
+        <Header />
     )
 }
--- react-js-scrimba-vite/src/components/Header/Header.jsx
+++ react-js-scrimba-vite/src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
 import styles from './Header.module.css'
-import globalIcon from '../../assets/Globe.svg'
 
 export default function Header() {
     return (
-        <header className={styles.headerContainer}>
-            <img src={globalIcon} className={styles.headerIcon} alt="header-icon" />
-            <span className={styles.headerText}>my travel journal.</span>
+        <header>
+            <p>test</p>
         </header>
     )
 }
